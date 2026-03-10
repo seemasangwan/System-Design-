aad28117ba35b12e9bcc41df482ba626f79af045
Seema <104920728+seemasangwan@users.noreply.github.com>
2026-03-11 00:23:12 +0530

    Add files via upload

---
+++ design patterns/creational design pattern/factory design pattern/shape/Shape.java
@@ -0,0 +1,14 @@
+/**
+ * shape interface
+ * */
+public interface Shape {
+
+    /**
+     * return the area of the shape
+     * */
+    public int getArea();
+    /**
+     * return the parameter if shape
+     * */
+    public int getPerimeter();
+}
---
+++ design patterns/creational design pattern/factory design pattern/shape/Square.java
@@ -0,0 +1,26 @@
+public class Square implements Shape {
+    private int side;
+    public Square(int side)
+    {
+        this.side=side;
+    }
+    public int getSide()
+    {
+        return this.side;
+    }
+    public void setSide(int side)
+    {
+        this.side=side;
+    }
+
+    @Override
+    public int getArea()
+    {
+        return side*side;
+    }
+    @Override
+    public int getPerimeter()
+    {
+        return 4*side;
+    }
+}
